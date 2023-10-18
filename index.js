66990b2cdffeea9e29fe623fd79c2d3cbed760e1
Dylan Conway <dylan.conway567@gmail.com>
2023-10-17 21:26:16 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello install test!");
+import fs from "fs/promises";
+import path from "path";
+console.log("moooooo");
+await fs.writeFile(path.join(import.meta.dir, "moo.txt"), "moooooo");
